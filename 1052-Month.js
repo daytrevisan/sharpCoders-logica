d5ef285708f9c15ea5653b6d67ca74d7b4560396
Dayane <dayanetm@outlook.com>
2023-11-01 12:42:44 -0300

    logica-js

---
+++ 1052-Month.js
@@ -0,0 +1,34 @@
+// Ler um número inteiro entre 1 e 12
+// Retornar o nome do mês correspondente com a 1a letra em maiúscula
+
+let mes = [
+    [1, "janeiro"],
+    [2, "fevereiro"],
+    [3, "marco"],
+    [4, "abril"],
+    [5, "maio"],
+    [6, "junho"],
+    [7, "julho"],
+    [8, "agosto"],
+    [9, "setembro"],
+    [10, "outubro"]
+    [11, "novembro"]
+    [12, "dezembro"]
+]
+
+// let month = parseInt(lines.shift());
+// switch(month){
+//     case 1: console.log("January"); break;
+//     case 2: console.log("February"); break;
+//     case 3: console.log("March"); break;
+//     case 4: console.log("April"); break;
+//     case 5: console.log("May"); break;
+//     case 6: console.log("June"); break;
+//     case 7: console.log("July"); break;
+//     case 8: console.log("August"); break;
+//     case 9: console.log("September"); break;
+//     case 10: console.log("October"); break;
+//     case 11: console.log("November"); break;
+//     case 12: console.log("December"); break;
+//     default: "Month undefined"
+// }
