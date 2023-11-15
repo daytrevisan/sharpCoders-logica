acda7b76be98333e1f801e9f6ac94ec24efc5cc0
Dayane <dayanetm@outlook.com>
2023-11-15 14:05:04 -0300

    logica-js

---
+++ 1186-BelowTheSecundaryDiagonal.js
@@ -0,0 +1,38 @@
+// Leia um caracter maiúsculo que indica a operação a ser performada (soma ou média) e aplique a operação a todos os números que estão abaixo da diagnonal secundária do array bidimensional M[12][12]
+// Retornar o cálculo da soma (S) ou da média (M) de todos os elementos da coluna em questão
+/*
+Input:
+S
+5.0
+0.0
+-3.5
+2.5
+4.1
+...
+Output:
+12.6
+*/
+
+let input = ['S', 5.0, 0.0, -3.5, 2.5, 4.1];
+
+const operacao = input.shift();
+
+let soma = 0;
+let cont = 0;
+
+for(let i=0; i<12; i++) {
+
+    for(let j=0; j<12; j++) {    
+        const numero = parseFloat(input.shift());
+        //console.log(numero);
+
+        if(i > 11-j) {
+            soma += numero;
+            cont += 1;
+        }     
+    }    
+}
+
+let resultado = operacao.toUpperCase() === 'S' ? parseFloat(soma) : parseFloat(soma/cont);
+
+console.log(resultado.toFixed(1))
